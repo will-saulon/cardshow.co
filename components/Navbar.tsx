27c8f069551f3a97e2fd0aff2ea94fadf117ef65
Ethan Salonga <ethansalonga28@gmail.com>
2025-03-18 17:44:45 -0400

    feat: add additional links to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,11 @@ export default function Navbar() {
     <nav className="w-full bg-white text-black px-4 py-3 flex justify-between">
       {/* Insert logo here */}
       <div>LOGO</div>
-      <a href="/login">Login</a>
+      <div className="flex gap-8">
+        <a href="/events" className="hidden sm:block">Events</a>
+        <a href="/create-event" className="hidden sm:block">Create Events</a>
+        <a href="/login" className="text-green-900 font-semibold">Login</a>
+      </div>
     </nav>
   )
 }
